dcc2add613c824b795226400e02ae7423a6dae81
Felipe Bello Dultra <felipe_bello_dultra@hotmail.com>
2020-09-24 17:15:04 -0300

    fix: correct bugs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
+import logoReact from './assets/logo.svg';
+
+import './App.css';
+
 function App() {
-  return <div />;
+  return <img className="App-logo" src={logoReact} alt="Logo React" />;
 }
 
 export default App;
